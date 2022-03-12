5e37651fad5f9c642b364192daa262921988ea78
Raimundo Neto <54272556+190718@users.noreply.github.com>
2022-03-12 11:31:57 -0300

    documentando

--- aulas/exemplo.py
+++ aulas/exemplo.py
@@ -1,9 +1,20 @@
+# Variaveis.
+# Tipo var.
+
 nome = 'Pedro'
 num = 7.885
 
 print(nome)
 print(num)
 
+# Variaveis.
+# Tipo float (para numeros flutuantes).
+
 novoNum = float(input('Novo numero: '))
 print(nome)
 print(novoNum)
+
+# outras variaveis
+# str (String, para tipo texto).
+# int (para numeros inteiros).
+# bool (booleano, para verdadeiro e/ou falso).
