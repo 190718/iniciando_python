ebb610050a1397b70051d9a644230133c4ca34a8
Raimundo Neto <54272556+190718@users.noreply.github.com>
2022-03-15 06:06:34 -0300

    import, nova linha if, ajuste texto

--- aulas02/aulas03.py
+++ aulas02/aulas03.py
@@ -1,12 +1,16 @@
 # Mostrando se o ano é Bissexto ou não.
 
+# Importando biblioteca
+from datetime import date
+
 # Entrando com dados do tipo inteiro para o ano.
 ano = int(input('Informa o Ano: '))
 # Calculo para saber se o resto da divisão é igual a 0 (zero).
-bissexto = ano % 4
+if ano == 0:
+    ano = date.today().year
 # Condição para informar se ano é bissexto ou não.
-if bissexto == 0 and ano % 100 != 0 or ano % 400 == 0:
-    print('Bissexto')
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('{} é Bissexto'.format(ano))
 # Condicional caso não for bissexto.
 else:
-    print('Ano normal!')
+    print('{} é um ano normal!'.format(ano))
