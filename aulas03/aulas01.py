9cf13ef16b20492e904c711ba4fcef09e3f2554b
Raimundo Neto <54272556+190718@users.noreply.github.com>
2022-03-15 06:44:45 -0300

    cores terminal

---
+++ aulas03/aulas01.py
@@ -0,0 +1,7 @@
+# Cores no terminal.
+print('\033[1;33;41m teste\033[m')
+print('\033[1;31;45m teste02\033[m')
+print('\033[1;30;40m teste03\033[m')
+print('\033[1;36;47m teste04\033[m')
+print('\033[1;32;43m teste05\033[m')
+print('\033[1;37;44m teste06\033[m')
