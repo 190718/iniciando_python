81b253af09a5171cb86e5891a98e8fcde4a6211d
Raimundo Neto <54272556+190718@users.noreply.github.com>
2022-03-15 06:00:32 -0300

    and != or

--- aulas02/aulas03.py
+++ aulas02/aulas03.py
@@ -5,9 +5,7 @@ ano = int(input('Informa o Ano: '))
 # Calculo para saber se o resto da divisão é igual a 0 (zero).
 bissexto = ano % 4
 # Condição para informar se ano é bissexto ou não.
-if bissexto == 0:
-# Segundo calculo para  confirmar se ano é bissexto.
-    mais1 = ano % 100
+if bissexto == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('Bissexto')
 # Condicional caso não for bissexto.
 else:
